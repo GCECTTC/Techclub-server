ed1cd8cf62e111310d27d37ad4c7dd6789c78e04
AYUSH JHA <72190187+A-jha383@users.noreply.github.com>
2021-10-27 09:21:35 +0530

    update

--- config/whitelist.js
+++ config/whitelist.js
@@ -1,3 +1,3 @@
-const whitelist = ["http://localhost:3000", "https://gcecttechclub.web.app"]
+const whitelist = ["http://localhost:3000", "https://gcecttechclub.web.app","https://bytemonk.gcect.in"]
 
 module.exports = whitelist
